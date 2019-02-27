1a0c9e3cf7d38dc5959d225e4fae510902282983
Matt <maxwellmattryan@gmail.com>
2019-02-27 15:16:00 -0600

    Add test html file, add todo lists

--- JishoSearch/JLPT_Jisho_Search.py
+++ JishoSearch/JLPT_Jisho_Search.py
@@ -1,6 +1,18 @@
+# library imports
+from bs4 import BeautifulSoup
+import requests
+
 def main():
 
-    print("Template")
+    # todo: plan program design and modularize it
+    # todo: input errors / exceptions
+    
+    url = "https://jisho.org/"
+    jlptLevel = input("Please enter JLPT level: ")
+    print(url + " : " + jlptLevel)
+
+    pageResponse = requests.get(url, verify = False)
+    print(pageResponse)
 
 if __name__ == "__main__":
   main()
