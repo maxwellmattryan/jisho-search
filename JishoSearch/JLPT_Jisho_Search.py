b3fe4a6619d588c7c6bddc22ee6359fe7c483d79
Matt <maxwellmattryan@gmail.com>
2019-02-28 18:03:43 -0600

    Add input error handling

--- JishoSearch/JLPT_Jisho_Search.py
+++ JishoSearch/JLPT_Jisho_Search.py
@@ -1,18 +1,44 @@
-# library imports
+# TODO:
+# plan program design (identify part of speech, is kanji usually used, etc)
+# input error handling for any input
+
+# libraries
+import string
 from bs4 import BeautifulSoup
 import requests
 
+def isValidJLPT(level):
+    level = level.lower()
+    if(level == "n5" or level == "n4" or level == "n3" or level == "n2" or level == "n1"):
+        return True
+    elif(level == "five" or level == "four" or level == "three" or level == "two" or level == "one"):
+        return True
+    else:
+        return False
+
 def main():
-
-    # todo: plan program design and modularize it
-    # todo: input errors / exceptions
     
-    url = "https://jisho.org/"
     jlptLevel = input("Please enter JLPT level: ")
-    print(url + " : " + jlptLevel)
+    while(isValidJLPT(jlptLevel) == False):
+        print("ERROR: " + "\"" + jlptLevel + "\" is an invalid input" + "\n")
+        jlptLevel = input("Please enter JLPT level: ")
 
-    pageResponse = requests.get(url, verify = False)
-    print(pageResponse)
+    url = "https://jisho.org" + "/search/jlpt%20" + jlptLevel + "%20%23words"
+    print(url)
 
-if __name__ == "__main__":
-  main()
+    pageResponse = requests.get(url)
+    soup = BeautifulSoup(pageResponse.text, "html.parser")
+
+    data = []
+    div = soup.find('div', { 'class' : 'concept_light clearfix'})
+
+    for furigana in div.find('span', { 'class' : 'furigana' }):
+        print(furigana)
+
+    for kanji in div.find('span', { 'class' : 'text' }):
+        print(kanji)
+
+    for meaning in div.find('span', { 'class' : 'text' }):
+        print(meaning)
+
+main()
