73d512c63d6bbf17f37d23ae3898908b18eb078e
Matt <maxwellmattryan@gmail.com>
2019-03-01 09:37:14 -0600

    Add changeInput (accepts numbers in word form)

--- JishoSearch/JLPT_Jisho_Search.py
+++ JishoSearch/JLPT_Jisho_Search.py
@@ -8,24 +8,52 @@ import string
 from bs4 import BeautifulSoup
 import requests
 
-# function for getting input,called in soup object declaration
+# word class
+class Word():
+    # initialize function
+    def __init__(self):
+        self.furigana = ""
+        self.kanji = ""
+        self.meaning = ""
+        self.isCommon = False
+        self.type = ""
+
+# function for getting input, called in soup object declaration
 def getURL():
     jlptLevel = input("Please enter JLPT level: ")
+    jlptLevel = changeInput(jlptLevel)
     while(isValidJLPT(jlptLevel) == False):
         print("ERROR: " + "\"" + jlptLevel + "\" is an invalid input" + "\n")
         jlptLevel = input("Please enter JLPT level: ")
+        jlptLevel = changeInput(jlptLevel)
     url = "https://jisho.org" + "/search/jlpt%20" + jlptLevel + "%20%23words"
-    # print tests
+
+    # print tests, delete later
     print(jlptLevel)
     print(url)
+
     return (url)
+
+# if user entered number in word form, change for proper search results
+def changeInput(level):
+    temp = level.lower()
+    if(temp == "one"):
+        return "N1"
+    elif(temp == "two"):
+        return "N2"
+    elif(temp == "three"):
+        return "N3"
+    elif(temp == "four"):
+        return "N4"
+    elif(temp == "five"):
+        return "N5"
+    else:
+        return level
 
 # checking if proper JLPT level
 def isValidJLPT(level):
     level = level.lower()
     if(level == "n5" or level == "n4" or level == "n3" or level == "n2" or level == "n1"):
-        return True
-    elif(level == "five" or level == "four" or level == "three" or level == "two" or level == "one"):
         return True
     else:
         return False
